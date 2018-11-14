92e4bbc64fe17fcdeeba4fdf1b11d4ada4e2f940
daigo.hiruma <daigo.hiruma@GX1845-2.local>
2018-11-15 07:15:03 +0900

    document readyを入れバグ修正

--- jquery_test/js/image_click.js
+++ jquery_test/js/image_click.js
@@ -1,6 +1,7 @@
-$("li[data-imagename]").on("click", function(){
-  var img_src = $(this).attr("data-imagename");
-  console.log(img_src);
-  $("#image_display").attr("src","image/"+img_src)
+$(document).ready(function(){
+  $("li[data-imagename]").on("click", function(){
+    var img_src = $(this).attr("data-imagename");
+    console.log(img_src);
+    $("#image_display").attr("src","image/"+img_src)
+  });
 });
-
